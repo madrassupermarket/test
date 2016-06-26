22e94444a673818f4e9006022da6d9170eb5accf
madrassupermarket <albert_stephen@yahoo.com>
2016-06-26 15:34:24 +0530

    added my first project to GIT

---
+++ src/episample.base/BlockTypes/ContentBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace episamplebase.BlockTypes
+{
+    [ContentType(DisplayName = "ContentBlock", GUID = "d9f0e538-737a-4c15-be3a-75b26aff8abe", Description = "")]
+    public class ContentBlock : BlockData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main Body",
+            Description = "Main body",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
---
+++ src/episample/Controllers/Blocks/ContentBlockController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+using episamplebase.BlockTypes;
+
+namespace episample.Controllers.Blocks
+{
+    public class ContentBlockController : BlockController<ContentBlock>
+    {
+        public override ActionResult Index(ContentBlock currentBlock)
+        {
+            return PartialView(currentBlock);
+        }
+    }
+}
